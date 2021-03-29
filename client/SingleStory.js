ba3245d548e67c068c3c8ef1aa9a774f193ce636
Vasyl Semak <vasylsmk1401@gmail.com>
2021-03-29 18:34:47 -0400

    refactor SingleStory: use react hooks

--- client/SingleStory.js
+++ client/SingleStory.js
@@ -1,39 +1,34 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Comments from './Comments'
 
-class SingleStory extends Component {
-  constructor() {
-    super()
-    this.state = { story: {} }
-  }
+const SingleStory = props => {
+  const [ story, setStory ] = useState({})
+  const content = story.content || ''
+  const comments = story.comments || []
 
-  async componentDidMount() {
-    try {
-      const id = this.props.match.params.id
-      const { data } = await axios.get(`/api/stories/${id}`)
-      this.setState({ story: data })
-    } catch (error) { console.error(error) }
-  }
+  useEffect(() => {
+    (async function() {
+      try {
+        const id = props.match.params.id
+        const { data } = await axios.get(`/api/stories/${id}`)
+        setStory(data)
+      } catch (error) { console.error(error) }
+    })()
+  }, [])
 
-  render() {
-    const { story } = this.state
-    const content = story.content || ''
-    const comments = this.state.story.comments || []
-
-    return (
-      <div id='single-story' className='column'>
-        <h1>{story.title}</h1>
-        {
-          content
-            .split('\n')
-            .map((line, idx) => <p key={idx}>{line}</p>)
-        }
-        <h3>Responses:</h3>
-        <Comments comments={comments} />
-      </div>
-    )
-  }
+  return (
+    <div id='single-story' className='column'>
+      <h1>{story.title}</h1>
+      {
+        content
+          .split('\n')
+          .map((line, idx) => <p key={idx}>{line}</p>)
+      }
+      <h3>Responses:</h3>
+      <Comments comments={comments} />
+    </div>
+  )
 }
 
 export default SingleStory
